b711db48f9e40a3ffc21539f306fb52f4d6192fc
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-04-06 21:47:14 +0100

    First commit.

---
+++ src/player.js
@@ -0,0 +1,24 @@
+'use strict';
+
+class Player {
+    constructor() {
+        this.name = 'Player';
+        this.level = 0;
+        this.health = 100;
+        this.XP = 0;
+        this.weapon = 'Sword';
+
+        this.pickUpWeapon = this.pickUpWeapon.bind(this);
+        this.fight = this.fight.bind(this);
+    }
+
+    pickUpWeapon() {
+        return true;
+    }
+
+    fight() {
+        return false;
+    }
+}
+
+export default Player;
