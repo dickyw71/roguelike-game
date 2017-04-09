673bb92b7d32fbcf49f4ed1e6391ec7b19ef7fb6
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-04-09 21:33:23 +0100

    First commit

---
+++ src/dungeon.js
@@ -0,0 +1,26 @@
+'use strict';
+import React, { Component } from 'react';
+
+class Dungeon extends Component {
+    constructor(props) {
+        super(props);
+
+    };
+    render() {
+        return (
+            <div className="Dungeon">
+            </div>
+        );
+    }
+}
+
+class Room extends Component {
+    constructor(props) {
+        super(props);
+        this.width = Math.floor(Math.random() * 100);
+        this.height = Math.floor(Math.random() * 100);
+    }
+}
+
+export { Room };
+export default Dungeon;
