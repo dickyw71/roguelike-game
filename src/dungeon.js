e38380ab477aed43729a4534cd4134cca136bf69
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-04-11 22:08:17 +0100

    Added render function to Room component.

--- src/dungeon.js
+++ src/dungeon.js
@@ -20,6 +20,13 @@ class Room extends Component {
         this.width = Math.floor(Math.random() * 100);
         this.height = Math.floor(Math.random() * 100);
     }
+
+    render() {
+        return (
+            <div className="Room">
+            </div>    
+        );
+    }
 }
 
 export { Room };
