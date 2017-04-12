5034f7df153a3f8ed93a8878a9507b2777a8e28b
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-04-12 15:39:18 +0100

    First commit.

---
+++ src/roguelike-game.js
@@ -0,0 +1,21 @@
+'use strict';
+import React, { Component } from 'react';
+
+class RoguelikeGame extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    generateDungeon() {
+        
+    }
+
+    render() {
+        return (
+            <div className="RoguelikeGame"></div>
+        );
+    }
+}
+
+export default RoguelikeGame;
