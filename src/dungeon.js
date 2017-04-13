efdc8895838e34f0e81c71e89a0f834a51e18ae2
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-04-13 08:21:06 +0100

    removed use strict and add calculate number of rooms.

--- src/dungeon.js
+++ src/dungeon.js
@@ -1,18 +1,39 @@
-'use strict';
 import React, { Component } from 'react';
+
+let style = {
+    room: {
+        border: "1px solid black",
+        margin: "5px" 
+    }
+};
 
 class Dungeon extends Component {
     constructor(props) {
         super(props);
-
+        
+        this.calculateRandomNoOfRooms = this.calculateRandomNoOfRooms.bind(this);
     };
+
+    calculateRandomNoOfRooms() {
+        let max = 15;   // Dungeons can have upto 15 rooms
+        let min = 5;    // Dungeons have at least 5 rooms
+        return Math.floor(Math.random() * (max-min)+1) + min;
+    }
+
     render() {
+        const rooms = [];
+        for(let i=0; i < this.calculateRandomNoOfRooms()+1; i++) {
+            rooms.push(<Room id="room" key={i.toString()} style={style.room}></Room>);
+        }
+        console.log(rooms.length);
         return (
             <div className="Dungeon">
+                {rooms}    
             </div>
         );
     }
 }
+
 
 class Room extends Component {
     constructor(props) {
@@ -23,11 +44,12 @@ class Room extends Component {
 
     render() {
         return (
-            <div className="Room">
+            <div className="RoomContents" style={style.room}>
             </div>    
         );
     }
 }
+
 
 export { Room };
 export default Dungeon;
