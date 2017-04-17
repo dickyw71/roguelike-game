6c9b392b9d510c3c474e9f03b33eab5dd99658e3
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-04-17 17:24:54 +0100

    Initial commit.

---
+++ src/player-status.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Player from './player.js';
+
+class PlayerStatus extends Component {
+
+    render() {
+        let player = new Player();
+        return {
+            
+        }
+    }
+}
