aaba6aae4c597f5fc90573a7574d1fe0a19f1c47
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-10-17 21:32:10 +0100

    Replaced inline style objects with css file and factored out components.

--- src/player-status.js
+++ src/player-status.js
@@ -1,35 +1,27 @@
 import React, { Component } from 'react';
 import Player from './player.js';
 import { Grid, Row, Col } from 'react-bootstrap';
+import "./player-status.css";
 
-let style = {
-    playerStatusPanel: {
-       // display: "flow",
-        position: "fix"
-    },
-    healthStatus: {
-        display: "inline-block"
-    }
-}
 class PlayerStatus extends Component {
 
     render() {
         let player = new Player();
         return (
-            <div className="playerStatus" style={style.playerStatusPanel}>
-                <Grid fluid="true">
+            <div className="playerStatus">
+                <Grid fluid={true}>
                     <Row className="show-grid">
                         <Col md={3}>
-                            <HealthStatus style={style.healthStatus}></HealthStatus>
+                            <HealthStatus/>
                         </Col>
                         <Col md={3}>
-                           <h3><strong>Level:</strong> 45</h3>
+                            <Level/>
                         </Col>
                         <Col md={3}>    
-                            <h3>Weapon: 66</h3>
+                            <Weapon/>
                         </Col>
                         <Col md={3}>
-                            <h3>XP: 33</h3>
+                            <XP/>
                         </Col>
                     </Row>
                 </Grid>
@@ -42,8 +34,36 @@ class HealthStatus extends Component {
 
     render() {
         return (
-            <h3>Health: 1000</h3>
+            <h3 className="healthStatus">Health: 1000</h3>
         )
     }
 }
+
+class Level extends Component {
+
+    render() {
+        return (
+            <h3 className="level"><strong>Level:</strong> 45</h3>
+        )
+    }
+}
+
+class Weapon extends Component {
+
+    render() {
+        return (
+            <h3 className="weapon">Weapon: 66</h3>
+        )
+    }
+}
+
+class XP extends Component {
+  
+    render() {
+        return (
+            <h3 className="xp">XP: 33</h3>
+        )
+    }
+}
+
 export default PlayerStatus; 
