681d2ee701d4dfd41a94b548994d3830cbf09a44
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-10-23 08:01:50 +0100

    Added defaultGameState function.

--- src/roguelike-game.js
+++ src/roguelike-game.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import Dungeon from './dungeon.js';
 import PlayerStatus from './player-status.js'
 
+const defaultGameState = () => {
+    
+}
+
+
 class RoguelikeGame extends Component {
  
     generateDungeon() {
