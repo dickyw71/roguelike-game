be56ef384ac5f9f3df15d6d9577963f8daf8d977
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-10-23 07:59:48 +0100

    Removed cruft.

--- src/dungeon.js
+++ src/dungeon.js
@@ -1,49 +1,25 @@
 import React, { Component } from 'react';
+import Player from './player.js';
 import './dungeon.css';
 
-let style = {
-    dungeon: {
-        border: "1px solid black",
-        margin: "10px",
-        width: "900px",
-        height: "900px",
-        background: "#CCCCCC"
-    },
-    room: {
-        margin: "10px 10px",
-        width: "40px",  //  default, will be changed by object
-        height: "40px",  //  default, will be changed by object
-        background: "#FFFFFF"
-    }
-};
 
 class Dungeon extends Component {
     constructor(props) {
         super(props);
-        
-        this.calculateRandomNoOfRooms = this.calculateRandomNoOfRooms.bind(this);
-    };
 
-    calculateRandomNoOfRooms() {
-        let max = 30;   // Dungeons can have upto 30 rooms
-        let min = 10;    // Dungeons have at least 10 rooms
-        return Math.floor(Math.random() * (max-min)+1) + min;
-    }
+     };
 
     render() {
-        const rooms = [];
-        for(let i=0; i < this.calculateRandomNoOfRooms()+1; i++) {
-            rooms.push(<Room id="room" key={i.toString()} style={style.room}></Room>);
-        }
-    
+  
         return (
-            <div className="Dungeon" style={style.dungeon}>
-                {rooms}    
+            <div 
+            className="dungeon"
+            >
+                <Player />
             </div>
         );
     }
 }
-
 
 class Room extends Component {
     constructor(props) {
@@ -53,22 +29,37 @@ class Room extends Component {
         this.width = Math.floor(Math.random() * (max - min) + min);
         this.height = Math.floor(Math.random() * (max - min) + min);
         this.margin = Math.floor(Math.random() * 100).toString() + "px 10px";
-    }
 
+        console.log(this.width, this.height, this.margin);
+    }
+    
     render() {
         style.room.width = this.width;
         style.room.height = this.height;
         style.room.margin = this.margin
         return (
             <div 
-                className="Room" 
-                style={style.room} 
+            className="room" 
+            style={style.room}
             >
-            </div>    
+                <HealthItem />
+            </div>
         );
     }
 }
 
+class HealthItem extends Component {
+    render() {
+        return (
+            <div 
+            className="healthItem" 
+            style={style.healthItem}
+            >
+            🗝
+            </div>
+        )
+    }
+}
 
 export { Room };
 export default Dungeon;
