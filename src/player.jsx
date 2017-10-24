f029f34d38a1e863bdaa84a3c9a3201c908124fe
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-10-24 07:32:55 +0100

    Renamed with jsx.

--- src/player.jsx
+++ src/player.jsx
@@ -51,6 +51,29 @@ class Player extends Component {
         }
     }
 
+    handleKeydown = (e) => {
+        let newDirection;
+        
+        switch(e.keyCode) {
+            case 37:
+                newDirection = { top: 0, left: -1 , dir: LEFT};
+                break;
+            case 38:
+                newDirection = { top: -1, left: 0 , dir: UP};
+                break;
+            case 39:
+                newDirection = { top: 0, left: 1, dir: RIGHT};
+                break;
+            case 40:
+                newDirection = { top: 1, left: 0, dir: DOWN };
+                break;
+            default:
+                return;
+        }
+
+        this.props.handlePlayerMovement(newDirection);
+    }
+
     render() {
         return (
             <div className="player"
